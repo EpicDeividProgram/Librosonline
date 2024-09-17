ce3734759f06dd87890a82fa06ae00f72da5d76a
nataliamoraluq <mora.luq.natalia68@gmail.com>
2024-09-17 16:47:39 -0400

    User model (entity) !: without the FK of type-of-user entity

---
+++ API/Components/User/Model.js
@@ -0,0 +1,41 @@
+// --------------------------------------------------------
+// ----- USER --- (MODEL) -----
+// --------------------------------------------------------
+// ---*** IMPORTACIONES ***---
+import {sequelize} from '../../Configuration/connection.js';
+import {Sequelize} from 'sequelize';
+
+export const User = sequelize.define('user', 
+{
+    idU: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true
+
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    birthDate: {
+        type: Sequelize.DATETIME,
+        allowNull: false
+    },
+    address: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    typeOfUser: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+      
+});
