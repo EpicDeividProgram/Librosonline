db17b4b9f0bb88dd4dcf0d35091f69c59643bd66
nataliamoraluq <mora.luq.natalia68@gmail.com>
2024-10-03 10:06:24 -0400

    testing (me equivoque de rama, yo (nat) lo subi en la de david y era en la test :()

--- API/Configuration/connection.js
+++ API/Configuration/connection.js
@@ -2,7 +2,10 @@
 // ----- CONNECTION MODULE --- (DB CONFIG CONNECTION) -----
 // --------------------------------------------------------
 import {Sequelize} from 'sequelize';
+//
 export const sequelize = new Sequelize('librosonline_db', 'root', 'Hormiga123', {
     dialect: 'mysql',
     host: 'localhost'
 });
+
+//new thingy
